5731aca92ccd3b2bdbe80f9609481669d6880be4
Bikash888 <bikashdulal150@gmail.com>
2020-04-05 13:34:04 +0545

    testing

--- src/main/java/com/csrfwork/project/CsrfWorkApplication.java
+++ src/main/java/com/csrfwork/project/CsrfWorkApplication.java
@@ -3,6 +3,8 @@ package com.csrfwork.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.csrfwork.project.security.SecurityConfiguration;
+
 @SpringBootApplication
 public class CsrfWorkApplication {
 
--- src/main/java/com/csrfwork/project/security/SecurityConfiguration.java
+++ src/main/java/com/csrfwork/project/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.csrfwork.project.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,12 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	
+	 @Override
+	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	    //    auth.inMemoryAuthentication().withUser("guest").password("{noop}guest1234").roles("USER");
+	        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin1234").roles("ADMIN");
+	    }
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -18,7 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .formLogin().disable()
         
         .httpBasic();
-	//http.csrf().disable();	
+	
 http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 
